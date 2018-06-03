671ec32c695eb5681274030b3e0405bf5949b478
che603000/fpln <che603000@gmail.com>
2018-06-04 01:33:01 +0300

    1111111111

--- webpack.config.js
+++ webpack.config.js
@@ -177,6 +177,7 @@ module.exports = {
                 target: 'http://localhost:3000/',
                 secure: true,
                 changeOrigin: true
+
             },
 
             // '/images/*': {
